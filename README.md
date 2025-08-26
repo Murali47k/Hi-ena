65bfc9e926049405570661b77636905d352ae6b0
Murali47k <murali47karthick@gmail.com>
2025-08-26 18:37:46 +0530

    Initial commit: Added Hi-ena base structure

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hi-ena
+A local LAN-based chat and file-sharing app built with Python and PyQt.
---
+++ main.py
@@ -0,0 +1,5 @@
+from PyQt5.QtWidgets import QApplication, QLabel
+app = QApplication([])
+label = QLabel('Hello Hi-ena!')
+label.show()
+app.exec_()
